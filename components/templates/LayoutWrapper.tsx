2963c75d14600844312bfbf3325f68484dd340c5
kelvinpraises <kelvinpraises@gmail.com>
2023-12-13 12:09:38 +0100

    feat: add react query provider

--- components/templates/LayoutWrapper.tsx
+++ components/templates/LayoutWrapper.tsx
@@ -2,21 +2,26 @@
 import { ThorinGlobalStyles, lightTheme } from "@ensdomains/thorin";
 import React from "react";
 import { ThemeProvider } from "styled-components";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import MainNav from "../organisms/MainNav";
 import StyledComponentsRegistry from "./StyledComponentsRegistry";
 import RootProvider from "@/lib/providers/provider";
+
+const client = new QueryClient();
 
 const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <RootProvider>
       <StyledComponentsRegistry>
         <ThemeProvider theme={lightTheme}>
-          <div className="flex w-full flex-col items-center gap-12">
-            {/* <ThorinGlobalStyles /> */}
-            <MainNav />
-            {children}
-          </div>
+          <QueryClientProvider client={client}>
+            <div className="flex w-full flex-col items-center gap-12">
+              {/* <ThorinGlobalStyles /> */}
+              <MainNav />
+              {children}
+            </div>
+          </QueryClientProvider>
         </ThemeProvider>
       </StyledComponentsRegistry>
     </RootProvider>
