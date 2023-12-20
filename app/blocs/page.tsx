6560604a3ae8a6c521a2a925a33f65b8126541be
kelvinpraises <kelvinpraises@gmail.com>
2023-12-20 18:04:23 +0100

    feat: read correct blocs

--- app/blocs/page.tsx
+++ app/blocs/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import BlocsCover from "@/components/atoms/BlocsCover";
 import { blocABI } from "@/lib/contracts/data";
+import { Card } from "@ensdomains/thorin";
 import { gql, request } from "graphql-request";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import { Address, useContractReads } from "wagmi";
 import { sepolia } from "wagmi/chains";
@@ -24,11 +26,16 @@ interface MusicBloc {
   id: string;
 }
 
+interface BlocData {
+  result: [string, boolean, BigInt];
+}
+
 interface QueryData {
   newMusicBlocs: MusicBloc[];
 }
 
 export default function BlocExplorerPage() {
+  const [blocInfo, setBlocInfo] = useState<any[]>();
   const { data, isLoading, error } = useQuery<QueryData, Error>(
     "launches",
     async (): Promise<QueryData> => {
@@ -36,40 +43,42 @@ export default function BlocExplorerPage() {
       return response as QueryData;
     },
   );
-
   useContractReads({
-    contracts:
-      data?.newMusicBlocs.map(({ bloc }) => {
-        return {
-          abi: blocABI,
-          address: "0x59c2ada2aadf5d27da4c23acc4f41447c73ce63a",
-          function: "getBlocMetadata",
-          chainId: sepolia.id,
-        };
-      }),
-    watch: true,
+    contracts: data?.newMusicBlocs.map(({ bloc }) => {
+      return {
+        abi: blocABI,
+        address: bloc,
+        functionName: "getBlocMetadata",
+        chainId: sepolia.id,
+      };
+    }),
     enabled: !!data,
-    onSuccess: (datas) => {
-      console.log(data);
-      console.log(datas);
+    onSuccess: (data: BlocData[]) => {
+      data.map((bloc: BlocData) => {
+        const blocInfoCid = bloc.result[0];
+        const released = bloc.result[1];
+      });
     },
   });
 
   return (
     <div className="flex h-full w-full max-w-[1228px] flex-col gap-12">
       <BlocsCover />
-
       <div>
-        <h1>Blocs Addresses</h1>
-        {isLoading && <p>Loading...</p>}
-        {error && <pre>{error.message}</pre>}
-        {data && (
-          <ul>
-            {data.newMusicBlocs.map((blocs: MusicBloc) => (
-              <li key={blocs.bloc}>{blocs.bloc}</li>
-            ))}
-          </ul>
-        )}
+        <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2">
+          <h1 className="col-span-full">Blocs Addresses</h1>
+          {isLoading && <p className="col-span-full">Loading...</p>}
+          {error && <pre className="col-span-full">{error.message}</pre>}
+          {data && (
+            <>
+              {data.newMusicBlocs.map((blocs: MusicBloc) => (
+                <Card key={blocs.bloc} className="">
+                  {blocs.bloc}
+                </Card>
+              ))}
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
